b3d85bbac65219e78518059ffcfb9a561b7aef28
Ivan Kubrakov <kubrakov.i@gmail.com>
2012-11-01 00:35:25 +0200

    1. Remove jQuery from Front-end. 2. Dynamically load script library(after start chat) and styles.

--- public/media_support/js/support.js
+++ public/media_support/js/support.js
@@ -11,6 +11,7 @@ Chat.prototype = {
 		this.chatContainer = document.getElementById('chatContainer');
 		this.chatList = document.getElementById('supportChat');
 		this.chatSend = document.getElementById('messageInput');
+		this.cursor = document.getElementById('supportCursor');
 	},
 	includeStyle: function(){
 		var css = document.createElement('link');
@@ -19,13 +20,21 @@ Chat.prototype = {
 		var fcss = document.getElementsByTagName('link')[0];
 		fcss.parentNode.insertBefore(css, fcss);
 	},
+	includeLib: function(){
+		var js = document.createElement('script');
+		js.src = this.config.lib;
+		var fjs = document.getElementsByTagName('script')[0];
+		fjs.parentNode.insertBefore(js, fjs);
+
+		this.sendScreen();
+	},
 	widgetInsert: function(){
 		// insert widget in document
 		var body = document.getElementsByTagName("body")[0];
 		body.innerHTML += this.config.controlHhtml;
 	},
 	askChat: function(el){
-		this.sendScreen();
+		this.includeLib();
 		// change styles of widget block
 		var chat = this;
 		el.style.display = 'none';		
@@ -53,6 +62,10 @@ Chat.prototype = {
 	},
 	sendScreen: function(){
 		var chat = this;
+		if (typeof html2canvas == 'undefined'){
+			setTimeout(function(){ chat.sendScreen(); }, 500);
+			return;
+		}
 	
 		// send screen to support
 		html2canvas( [ document.body ], {
@@ -60,20 +73,23 @@ Chat.prototype = {
 				var width = canvas.getAttribute('width');
 				var height = canvas.getAttribute('height');
 				// base64
-				var data = canvas.toDataURL("image/png").replace(/^data:image\/png;base64,/,"");													
-
-				$.ajax({ 
-					type: 'POST',
-					url: '/image',
-					data: {
-						image: data,
-						name: Math.floor((Math.random()*1000)+1),
-					},
-                    dataType: 'json',
-					success: function(data) {
+				var data = canvas.toDataURL("image/png").replace(/^data:image\/png;base64,/,"");
+				
+				// ajax request																	
+				var http = new XMLHttpRequest();
+				var url = "/image";
+				var params = 'image='+encodeURIComponent(data)+'&name='+Math.floor((Math.random()*10000)+1)+'';
+				http.open('POST', url, true);
+				http.setRequestHeader('Accept', 'application/json, text/javascript');
+				http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+				http.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+				http.onreadystatechange = function() {
+					if(http.readyState == 4 && http.status == 200) {
+						var data = JSON.parse(http.responseText);
 						chat.socket.emit('message', { me: 'client', mytime: new Date(), data: [data.image, width, height], type: chat.typesMessages[3] });
 					}
-				});			
+				}
+				http.send(params);
 			}
 		});
 	},
@@ -86,17 +102,20 @@ Chat.prototype = {
 	addMessage: function(message, me){
 		var label = (me) ? this.config.langTextMe : this.config.langTextSupport;
 		var classMessage = (me) ? 'me' : 'support';
-		$(this.chatList).append('<li class="chat_item '+classMessage+'"><b>' + label + ':</b> ' + message + '</li>');
+		// append message
+		this.chatList.innerHTML = this.chatList.innerHTML + '<li class="chat_item '+classMessage+'"><b>' + label + ':</b> ' + message + '</li>';
 		// scroll
 		var objDiv = document.getElementById('supportChat');
 		objDiv.scrollTop = objDiv.scrollHeight;
 	},
 	showClick: function(coordinates){
 		clearTimeout(this.timeout);
-		$('.show_cursor').css({'top': coordinates[0]+'px', left: coordinates[1]+'px'});
-		$('.show_cursor').show();
+		this.cursor.style.top = coordinates[0]+'px';
+		this.cursor.style.left = coordinates[1]+'px';
+		this.cursor.style.display = 'block';
+		var chat = this;
 		this.timeout = setTimeout(function(){
-			$('.show_cursor').hide();
+			chat.cursor.style.display = 'none';
 		}, 5000);
 	},
 	listening: function(){
@@ -125,9 +144,10 @@ Chat.prototype = {
 };
 
 var FrontChat = new Chat({
-	controlHhtml: '<div class="chat_container" id="chatContainer"><a onClick="FrontChat.askChat(this)" class="start_chat">Служба поддержки</a><div class="waiting_label">Ожидаем оператора</div><ul class="chat_list" id="supportChat"></ul><input id="messageInput" type="text" class="send_message" /></div><div class="show_cursor"></div>',
+	controlHhtml: '<div class="chat_container" id="chatContainer"><a onClick="FrontChat.askChat(this)" class="start_chat">Служба поддержки</a><div class="waiting_label">Ожидаем оператора</div><ul class="chat_list" id="supportChat"></ul><input id="messageInput" type="text" class="send_message" /></div><div class="show_cursor" id="supportCursor"></div>',
 	langTextMe: 'Я',
 	langTextSupport: 'Оператор',
-	styles: 'media_support/css/styles.css'
+	styles: 'media_support/css/styles.css',
+	lib: 'media_support/js/html2canvas.min.js'
 });
 
