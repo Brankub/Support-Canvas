ba30af96d9a6b86a89b795fc17a033383bf8117d
Ivan Kubrakov <kubrakov.i@gmail.com>
2012-11-01 19:03:54 +0200

    Socket IO for all browsers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var static = require("node-static");
-var file = new(static.Server)('./public');
-var fs = require('fs');
-var qs = require('querystring');
+var static = require("node-static"),
+	fs = require('fs'),
+	qs = require('querystring'),
+	file = new(static.Server)('./public');
 
 var app = require('http').createServer(function (request, response) {
 	if (request.url === '/image') {
@@ -40,6 +40,19 @@ io.sockets.on("connection", function(socket){
   })
 })
 
+io.configure( function(){
+	io.enable('browser client minification');  // send minified client
+	io.enable('browser client etag');          // apply etag caching logic based on version number
+	io.enable('browser client gzip');          // gzip the file
+	io.set('log level', 1);                    // reduce logging
+	io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+	    'websocket'
+	  , 'flashsocket'
+	  , 'htmlfile'
+	  , 'xhr-polling'
+	  , 'jsonp-polling'
+	]);
+});
 
 
 app.listen(8080);
